3aaf4e3244d720d7ca94420f1ae0aa21c8d66c2e
KendrickPC <kennethpchang@gmail.com>
2017-05-06 17:12:05 +0800

    changed json get request to .username and .media

--- instagram.php
+++ instagram.php
@@ -1,6 +1,6 @@
 <?php
 $username = 'brotherkendrick';
-$json = file_get_contents('http://www.instagram.com/'_$username_'/media/');
+$json = file_get_contents('http://www.instagram.com/'.$username.'/media/');
 $instagram_feed_data = json_decode($json, true);
 
 ?>
